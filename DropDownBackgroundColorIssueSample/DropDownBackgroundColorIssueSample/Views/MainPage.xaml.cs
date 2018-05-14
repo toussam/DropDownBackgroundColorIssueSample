c1f0136304feecb2af5ef88f4bd99a362f0d9d6f
toussam <mathias.toussaint@outlook.com>
2018-05-14 13:44:45 +0200

    Reproduced the issue with the SfAutoComplete's DropDownBackgroundColor property when using an Android emulator 8.0 (API 26)

---
+++ DropDownBackgroundColorIssueSample/DropDownBackgroundColorIssueSample/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DropDownBackgroundColorIssueSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            AutoComplete.DataSource = new List<string>
+            {
+                "test1 ",
+                "test2 ",
+                "test3 ",
+                "test4 ",
+                "test5 ",
+            };
+        }
+    }
+}
